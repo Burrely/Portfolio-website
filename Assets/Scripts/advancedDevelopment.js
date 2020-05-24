07b3b4c6a3bd5ed2dd074382fa5ff81f5f12fee3
Burrely <dinosammy1@live.nl>
2020-05-24 08:28:26 +0200

    Implementation basic Three.js poc

--- Assets/Scripts/advancedDevelopment.js
+++ Assets/Scripts/advancedDevelopment.js
@@ -117,4 +117,31 @@ window.addEventListener('load', () => {
     });
 
     
+    // Three.js, not the fourth dimension.
+    var scene3D = new THREE.Scene();
+    var containerElement = document.getElementById("ThreeJSExample");
+    var camera = new THREE.PerspectiveCamera( 75, 640/320, 0.1, 1000 );
+
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize( 640, 320 );
+    containerElement.appendChild( renderer.domElement );
+
+    var geometry = new THREE.BoxGeometry();
+    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    var cube = new THREE.Mesh( geometry, material );
+    scene3D.add( cube );
+
+    camera.position.z = 5;
+
+    var animate = function () {
+        requestAnimationFrame( animate );
+
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+
+        renderer.render( scene3D, camera );
+    };
+    
+    animate();
+    
 });
